1c89c2c0783d9d1cc54695ffee676e807a7724aa
William Oliveira <202211240022@ifba.edu.br>
2025-01-23 00:01:38 -0300

    feat: add footer component with styling

---
+++ clique-vagas-frontend/app/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Footer.module.css';
+
+const Footer: React.FC = () => {
+    return (
+        <footer className={styles.footer}>
+            &copy; 2025 Clique Vagas
+        </footer>
+    );
+};
+
+export default Footer;
