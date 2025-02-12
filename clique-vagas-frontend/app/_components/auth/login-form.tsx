cfcbb49bf8cdfec700f98fc61b3be264f09dde21
Edmito <edmilsongandra10@gmail.com>
2025-02-12 20:45:40 -0300

    feat: add login page and login form component for user authentication

---
+++ clique-vagas-frontend/app/_components/auth/login-form.tsx
@@ -0,0 +1,47 @@
+import { cn } from '@/app/_lib/utils';
+import { Button } from '@/app/_components/ui/button';
+import { Input } from '@/app/_components/ui/input';
+import { Label } from '@/app/_components/ui/label';
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<'form'>) {
+  return (
+    <form className={cn('flex flex-col gap-6', className)} {...props}>
+      <div className="flex flex-col items-center gap-2 text-center">
+        <h1 className="text-2xl font-bold">Entrar na sua conta</h1>
+        <p className="text-balance text-sm text-muted-foreground">
+          Insira seu email abaixo para entrar na sua conta
+        </p>
+      </div>
+      <div className="grid gap-6">
+        <div className="grid gap-2">
+          <Label htmlFor="email">Email</Label>
+          <Input id="email" type="email" placeholder="m@example.com" required />
+        </div>
+        <div className="grid gap-2">
+          <div className="flex items-center">
+            <Label htmlFor="password">Senha</Label>
+            <a
+              href="#"
+              className="ml-auto text-sm underline-offset-4 hover:underline"
+            >
+              Esqueceu sua senha?
+            </a>
+          </div>
+          <Input id="password" type="password" required />
+        </div>
+        <Button type="submit" className="w-full">
+          Entrar
+        </Button>
+      </div>
+      <div className="text-center text-sm">
+        Não tem uma conta?{' '}
+        <a href="#" className="underline underline-offset-4">
+          Cadastre-se
+        </a>
+      </div>
+    </form>
+  );
+}
