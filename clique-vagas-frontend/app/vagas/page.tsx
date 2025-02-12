e69439e1401b49f98e8d9bc59f9de17699122d14
William Oliveira <202211240022@ifba.edu.br>
2025-02-12 20:29:18 -0300

    feat: create main Page component with Header, JobCard, and Footer

---
+++ clique-vagas-frontend/app/vagas/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import JobCard from "../components/JobCard";
+
+export default function Page() {
+  return (
+    <div>
+      <Header />
+      <JobCard />
+      <Footer />
+    </div>
+  );
+}
