478d25ce900e4be7377246661f5aea8ecb388d37
William Oliveira <202211240022@ifba.edu.br>
2025-02-14 18:52:49 -0300

    feat: update SkillPosting interface to include type property and change idSkill to number

--- clique-vagas-frontend/app/interfaces/Skill.ts
+++ clique-vagas-frontend/app/interfaces/Skill.ts
@@ -6,7 +6,8 @@ export interface Skill {
   
   export interface SkillPosting {
     id?: number;
-    name: string; // For display purposes. Does not go into the JSON
-    idSkill: Skill;
+    idSkill: number;
     proficiencyLevel: 'BASIC' | 'INTERMEDIATE' | 'ADVANCED' | 'EXPERT';
+    name: string; // For display purposes. Does not go into the JSON
+    type: 'HARD_SKILL' | 'SOFT_SKILL'; // Also for display purposes
   }
